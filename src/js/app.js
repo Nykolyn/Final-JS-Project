2dcb8db76d6ca129b6a6cbab908c73f3c0370c38
BohdanRadchenko <radchenkobs@gmail.com>
2019-07-04 10:57:05 +0300

    merge done

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,25 @@
-import {getFilms} from './services/api';
-import {createListItem} from './view';
-const films = getFilms().then(result => result.results.forEach(item => createListItem(item)));
-
-
-// ------------  TIME  -------------------- 
-setInterval(function () {  
-const date = new Date();
-let h = date.getHours();
-let m = date.getMinutes();
-let s = date.getSeconds();  
- h = (h < 10) ? '0' + h : h;  
- m = (m < 10) ? '0' + m : m;  
- s = (s < 10) ? '0' + s : s;  
-document.getElementById('time').innerHTML = h + ':' + m + ':' + s;  
-}, 1000); 
-
-
-
-import {
-    createListItem
-} from "./view";
 import Films from './Fims/Films';
 import {
     refs
 } from './constants';
+import {
+    getFilms
+} from './services/api';
+import {
+    createListItem
+} from './view';
+
+// ------------  TIME  -------------------- 
+setInterval(function () {
+    const date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    h = (h < 10) ? '0' + h : h;
+    m = (m < 10) ? '0' + m : m;
+    s = (s < 10) ? '0' + s : s;
+    document.getElementById('time').innerHTML = h + ':' + m + ':' + s;
+}, 1000);
 
 const films = new Films();
 
@@ -32,6 +28,8 @@ films.getFilms().then(result =>
 );
 
 // timer
+
+
 
 
 //modal card 
