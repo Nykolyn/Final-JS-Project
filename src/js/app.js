e3d35623ec49524fc78cf2dd06794a69445362fe
nikulinsergey <niku7in.s.s@gmail.com>
2019-07-04 09:44:02 +0300

    TIME

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,16 @@
 import {getFilms} from './services/api';
 import {createListItem} from './view';
-
 const films = getFilms().then(result => result.results.forEach(item => createListItem(item)));
+
+
+// ------------  TIME  -------------------- 
+setInterval(function () {  
+const date = new Date();
+let h = date.getHours();
+let m = date.getMinutes();
+let s = date.getSeconds();  
+ h = (h < 10) ? '0' + h : h;  
+ m = (m < 10) ? '0' + m : m;  
+ s = (s < 10) ? '0' + s : s;  
+document.getElementById('time').innerHTML = h + ':' + m + ':' + s;  
+}, 1000);  
