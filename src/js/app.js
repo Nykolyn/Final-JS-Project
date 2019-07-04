a0e6a726c4ed29f38c3bf46bb022e37b8a833c75
RZFalcore <box.rz.box@gmail.com>
2019-07-04 17:07:39 +0300

    Switching is working. Basic CSS

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,7 @@ import {
 import {
     createListItem
 } from './view';
+import {switchPages} from './switchPages';
 
 // ------------  TIME  -------------------- 
 setInterval(function () {
@@ -25,62 +26,62 @@ const films = new Films();
 
 films.getFilms().then(result =>
     result.forEach(item => createListItem(item))
-);
-
-// timer
-
-
-
-
-//modal card 
-// console.log(refs.filmsList);
-refs.filmsList.addEventListener('click', openCard);
-
-// function openCard(event) {
-//     const targetCard = event.target.closest('li');
-//     const targetDiv = targetCard.querySelector('.card-wrap');
-
-//     targetCard.classList.toggle('modal-card');
-//     targetDiv.classList.toggle('card-block');
-// }
-
-function openCard(event) {
-
-    const targetCard = event.target.closest('li');
-    const targetDiv = targetCard.querySelector('.card-wrap');
-    const exitButton = targetCard.querySelector('.exit-button');
-
-    const cardStyle = window.getComputedStyle(targetCard);
-    // console.log('cardStyle :', cardStyle);
-
-    // mouse cord
+    );
+    
+    // timer
+    
+    
+    
+    
+    //modal card 
+    // console.log(refs.filmsList);
+    refs.filmsList.addEventListener('click', openCard);
+    
+    // function openCard(event) {
+        //     const targetCard = event.target.closest('li');
+        //     const targetDiv = targetCard.querySelector('.card-wrap');
+        
+        //     targetCard.classList.toggle('modal-card');
+        //     targetDiv.classList.toggle('card-block');
+        // }
+        
+        function openCard(event) {
+            
+            const targetCard = event.target.closest('li');
+            const targetDiv = targetCard.querySelector('.card-wrap');
+            const exitButton = targetCard.querySelector('.exit-button');
+            
+            const cardStyle = window.getComputedStyle(targetCard);
+            // console.log('cardStyle :', cardStyle);
+            
+            // mouse cord
     const clientX = event.layerX;
     const clientY = event.layerY;
-
+    
     if (!targetCard.className.includes('modal-card')) {
         targetCard.classList.add('modal-card');
         targetDiv.classList.add('card-block');
-
+        
         window.scroll(0, 100);
-
+        
         //toggle event click 
         refs.filmsList.removeEventListener('click', openCard);
         refs.filmsList.addEventListener('click', closedCard);
-
+        
         function closedCard(event) {
             if (event.target === exitButton) {
                 targetCard.classList.remove('modal-card')
                 targetDiv.classList.remove('card-block');
-
+                
                 window.scroll(clientX, clientY);
-
+                
                 //toggle event click 
                 refs.filmsList.removeEventListener('click', closedCard);
                 refs.filmsList.addEventListener('click', openCard);
             }
         }
     }
-
+    
     console.log(event);
     // console.log('pageYOffset', pageYOffset);   
     // console.log(event.layerY);
---
+++ src/js/switchPages.js
@@ -0,0 +1,51 @@
+import {API, refs} from './constants';
+import {createListItem} from './view';
+
+const prvsBtn = document.querySelector('.prvs');
+const nextBtn = document.querySelector('.next');
+const buttonDiv = document.querySelector('.switchPages');
+
+buttonDiv.addEventListener('click', switchPages)
+prvsBtn.disabled = true;
+// prvsBtn.classList.add('disabled');
+
+// // if(prvsBtn.disabled = true ){
+// //     prvsBtn.classList.add('disabled')
+// // } else if (prvsBtn.disabled = false) {
+// //     prvsBtn.classList.remove('disabled')
+// // }
+
+let counter = 1;
+
+function switchPages (event)  {
+    refs.filmsList.innerHTML = '';
+    
+    if (event.target === nextBtn){
+        counter++;
+        prvsBtn.disabled = false;
+        return fetch (`https://api.themoviedb.org/3/movie/popular?api_key=027ca1d5e779abba9fcdc8b6b57f2385&language=en-US&page=${counter}`) 
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            data.results.map(el => createListItem(el));
+        })
+        
+        
+    }   else if(event.target === prvsBtn ){
+        counter -= 1;
+        if(counter <= 1 ){
+            counter = 1;
+            prvsBtn.disabled = true;
+        
+        }
+        return fetch (`https://api.themoviedb.org/3/movie/popular?api_key=027ca1d5e779abba9fcdc8b6b57f2385&language=en-US&page=${counter}`) 
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                data.results.map(el => createListItem(el));
+            });
+
+        
+    }
+
+}
