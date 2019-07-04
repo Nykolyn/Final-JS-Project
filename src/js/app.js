2c07c93c57d46078bd01bd8314d49fa8cd066a8e
BohdanRadchenko <radchenkobs@gmail.com>
2019-07-04 17:09:10 +0300

    before test

--- src/js/app.js
+++ src/js/app.js
@@ -53,7 +53,7 @@ function openCard(event) {
     const image = targetCard.querySelector('img');
 
     const filmListTitle = targetCard.querySelector('.film-list__title');
-    
+
 
     const cardStyle = window.getComputedStyle(targetCard);
     // console.log('cardStyle :', cardStyle);
@@ -68,17 +68,17 @@ function openCard(event) {
         // imageWrap.classList.add('image-wrap_markup')  //test
         image.classList.add('img-markup');
         filmListTitle.classList.add('display-none')
-        
+
         window.scroll(0, 100);
-        
+
         //toggle event click 
         refs.filmsList.removeEventListener('click', openCard);
         refs.filmsList.addEventListener('click', closedCard);
-        
+
         function closedCard(event) {
             console.log(event.target);
-            
-            
+
+
             if (event.target === exitButton || event.target === image || event.target === list || event.target.nodeName === 'IMG') {
                 targetCard.classList.remove('modal-card')
                 targetDiv.classList.remove('card-block');
