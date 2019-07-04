02733a3869b8b3b957d897a1670a953e13fd97cf
softok5 <softok.1990@gmail.com>
2019-07-04 12:49:08 +0300

    search2

--- src/js/Fims/Films.js
+++ src/js/Fims/Films.js
@@ -1,4 +1,4 @@
-import * as api from '../services/api'
+import * as api from '../services/api';
 
 export default class Films {
     constructor(films = []) {
@@ -16,4 +16,41 @@ export default class Films {
     updateComment(id, comment) {
         return api.commentFilm(id, comment).then()
     }
-}
+
+    searchFilm() {
+        return api.getFilms().then(films => {
+                 if(value !== ''){
+                    films.forEach(elen =>{
+                    if (elen.textContent.search(value) == -1){
+                        elen.classList.add('hide')
+                    }else {
+                        elen.class.remove('hide')
+                    }
+                    })
+                        
+                }else {
+                    films.forEach(elem => elem.classList.remove('hide'))
+        }
+    
+        })
+       
+    }
+
+
+    // }
+    // if(value !== ''){
+    //     list.forEach(elen =>{
+    //     if (elen.textContent.search(value) == -1){
+    //         elen.classList.add('hide')
+    //     }else {
+    //         elen.class.remove('hide')
+    //     }
+    //     })
+            
+    // }else {
+    //     list.forEach(elem => elem.classList.remove('hide'))
+    // }
+
+
+
+    }
---
+++ src/js/search.js
@@ -0,0 +1,20 @@
+// export const searchForm = (evt) => {
+//     const list = document.querySelectorAll('.list-item')
+//     const value = evt.target.value.trim()
+//     console.log(evt)
+    
+    
+//     // if(value !== ''){
+//     //     list.forEach(elen =>{
+//     //     if (elen.textContent.search(value) == -1){
+//     //         elen.classList.add('hide')
+//     //     }else {
+//     //         elen.class.remove('hide')
+//     //     }
+//     //     })
+            
+//     // }else {
+//     //     list.forEach(elem => elem.classList.remove('hide'))
+//     // }
+// }
+// // search.addEventListener('input',searchForm)
