ffce6545d7e202459609d96841475a9e003bc557
Roman Bezpalyi <romabespaly@gmail.com>
2019-07-04 17:15:21 +0300

    no result fixed

--- src/js/search.js
+++ src/js/search.js
@@ -16,14 +16,18 @@ export const onSearch = (event) => {
         fetch(`https://api.themoviedb.org/3/search/movie?api_key=027ca1d5e779abba9fcdc8b6b57f2385&language=en-US&query=${value}`).then(response => {
             if (response.ok) return response.json()
         }).then(films => {
-            if(films.results.length < 1) {
+            if (films.results.length < 1) {
                 console.log(refs.mainSection.children);
-                // refs.mainSection.removeChild('h2');
+                do {
+                    refs.mainSection.removeChild(refs.mainSection.lastChild);
+                } while (refs.mainSection.children.length > 1);
 
                 const noFilmText = createElementWithClass('h2', 'film-not-found');
                 noFilmText.textContent = "Sorry, no films are found... :(";
                 refs.mainSection.append(noFilmText)
-            } else {films.results.forEach(film => createListItem(film))}
+            } else {
+                films.results.forEach(film => createListItem(film))
+            }
         })
     } else {
         api.getFilms().then(films => films.results.forEach(film => createListItem(film)))
