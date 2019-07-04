7eae80b2dc53738abf0286f012e8111c3389633d
BohdanRadchenko <radchenkobs@gmail.com>
2019-07-04 15:53:25 +0300

    create style scss modal card

--- src/js/app.js
+++ src/js/app.js
@@ -52,6 +52,9 @@ function openCard(event) {
     const imageWrap = targetCard.querySelector('.image-wrap');
     const image = targetCard.querySelector('img');
 
+    const filmListTitle = targetCard.querySelector('.film-list__title');
+    
+
     const cardStyle = window.getComputedStyle(targetCard);
     // console.log('cardStyle :', cardStyle);
 
@@ -64,22 +67,24 @@ function openCard(event) {
         targetDiv.classList.add('card-block');
         // imageWrap.classList.add('image-wrap_markup')  //test
         image.classList.add('img-markup');
-
+        filmListTitle.classList.add('display-none')
+        
         window.scroll(0, 100);
-
+        
         //toggle event click 
         refs.filmsList.removeEventListener('click', openCard);
         refs.filmsList.addEventListener('click', closedCard);
-
+        
         function closedCard(event) {
             console.log(event.target);
-
-
+            
+            
             if (event.target === exitButton || event.target === image || event.target === list || event.target.nodeName === 'IMG') {
                 targetCard.classList.remove('modal-card')
                 targetDiv.classList.remove('card-block');
                 imageWrap.classList.remove('image-wrap_markup')
                 image.classList.remove('img-markup');
+                filmListTitle.classList.remove('display-none')
 
                 window.scroll(clientX, clientY);
 
