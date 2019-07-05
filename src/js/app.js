5e29f4c74a0ad17f6690a74c142f2f8884e41794
Anton Azhhirevych <azhhirevych943@gmail.com>
2019-07-05 16:53:52 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -27,59 +27,59 @@ films.getFilms().then(result => result.forEach(item => createListItem(item)));
 
 // timer
 
-
-
-
-//modal card 
+//modal card
 refs.filmsList.addEventListener('click', openCard);
 function openCard(event) {
+  const list = document.querySelector('.container');
+  const body = document.querySelector('body');
 
-    const list = document.querySelector('.container');
-    const body = document.querySelector('body');
+  const targetCard = event.target.closest('li');
+  const targetDiv = targetCard.querySelector('.card-wrap');
+  const exitButton = targetCard.querySelector('.exit-button');
+  const imageWrap = targetCard.querySelector('.image-wrap');
+  const image = targetCard.querySelector('img');
 
-    const targetCard = event.target.closest('li');
-    const targetDiv = targetCard.querySelector('.card-wrap');
-    const exitButton = targetCard.querySelector('.exit-button');
-    const imageWrap = targetCard.querySelector('.image-wrap');
-    const image = targetCard.querySelector('img');
+  const filmListTitle = targetCard.querySelector('.film-list__title');
 
-    const filmListTitle = targetCard.querySelector('.film-list__title');
+  const cardStyle = window.getComputedStyle(targetCard);
+  // console.log('cardStyle :', cardStyle);
 
-    const cardStyle = window.getComputedStyle(targetCard);
-    // console.log('cardStyle :', cardStyle);
+  // mouse cord
+  const clientX = event.layerX;
+  const clientY = event.layerY;
 
-    // mouse cord
-    const clientX = event.layerX;
-    const clientY = event.layerY;
+  if (!targetCard.className.includes('modal-card')) {
+    targetCard.classList.add('modal-card');
+    targetDiv.classList.add('card-block');
+    // imageWrap.classList.add('image-wrap_markup')  //test
+    image.classList.add('img-markup');
+    filmListTitle.classList.add('display-none');
 
-    if (!targetCard.className.includes('modal-card')) {
-        targetCard.classList.add('modal-card');
-        targetDiv.classList.add('card-block');
-        // imageWrap.classList.add('image-wrap_markup')  //test
-        image.classList.add('img-markup');
-        filmListTitle.classList.add('display-none')
+    window.scroll(0, 100);
 
-        window.scroll(0, 100);
+    //toggle event click
+    refs.filmsList.removeEventListener('click', openCard);
+    refs.filmsList.addEventListener('click', closedCard);
 
-        //toggle event click 
-        refs.filmsList.removeEventListener('click', openCard);
-        refs.filmsList.addEventListener('click', closedCard);
+    function closedCard(event) {
+      if (
+        event.target === exitButton ||
+        event.target === image ||
+        event.target === list ||
+        event.target.nodeName === 'IMG'
+      ) {
+        targetCard.classList.remove('modal-card');
+        targetDiv.classList.remove('card-block');
+        imageWrap.classList.remove('image-wrap_markup');
+        image.classList.remove('img-markup');
+        filmListTitle.classList.remove('display-none');
 
-        function closedCard(event) {
+        window.scroll(clientX, clientY);
 
-            if (event.target === exitButton || event.target === image || event.target === list || event.target.nodeName === 'IMG') {
-                targetCard.classList.remove('modal-card')
-                targetDiv.classList.remove('card-block');
-                imageWrap.classList.remove('image-wrap_markup')
-                image.classList.remove('img-markup');
-                filmListTitle.classList.remove('display-none')
-
-                window.scroll(clientX, clientY);
-
-                //toggle event click 
-                refs.filmsList.removeEventListener('click', closedCard);
-                refs.filmsList.addEventListener('click', openCard);
-            }
-        }
+        //toggle event click
+        refs.filmsList.removeEventListener('click', closedCard);
+        refs.filmsList.addEventListener('click', openCard);
+      }
     }
+  }
 }
