6d1929659afd977500e5ae0c7b8c6cc612b9fed7
Anton Azhhirevych <azhhirevych943@gmail.com>
2019-07-08 13:53:47 +0300

    added movie info to my favorite film

--- src/js/favorites.js
+++ src/js/favorites.js
@@ -7,9 +7,18 @@ const personalIdUser = sessionStorage.getItem('id');
 console.log('id', personalIdUser);
 const idUser = personalIdUser;
 
-// ДОДАЄМО В МОЇ УЛЮБЛЕНІ
 export const handleFavBtnClick = ({ target = { textContent } }) => {
   if (target.textContent === 'my movies') {
+    const release = target.closest('li').children[0].children[1].children[1].children[1]
+      .textContent;
+
+    const count = target.closest('li').children[0].children[1].children[1].children[2].textContent;
+
+    const average = target.closest('li').children[0].children[1].children[1].children[3]
+      .textContent;
+
+    const overview = target.closest('li').children[0].children[1].children[1].children[4]
+      .textContent;
     const src = target.closest('li').children[0].children[0].children[0].src;
     const title = target.closest('li').children[1].textContent;
     let result;
@@ -22,9 +31,13 @@ export const handleFavBtnClick = ({ target = { textContent } }) => {
     const film = {
       poster_path: result,
       title: title,
+      release_date: release,
+      vote_count: count,
+      vote_average: average,
+      overview: overview,
       idUser: idUser,
     };
-    // ПЕРЕВІРКА НА НАЯВНІСТЬ ФІЛЬМА В МАСИВІ
+
     getFilmsFavorite(idUser).then(result => {
       const resultSearch = result.some(film => film.title === title && film.idUser === idUser);
       !resultSearch ? saveFilm(film) : null;
@@ -46,7 +59,6 @@ export const handleFavBtnClick = ({ target = { textContent } }) => {
   }
 };
 
-//ВИХІД НА ГОЛОВНУ СТОРІНКУ
 function exitToFilm() {
   favorite.textContent = 'My Movies';
   refs.filmsList.innerHTML = '';
@@ -57,16 +69,16 @@ function exitToFilm() {
   favorite.addEventListener('click', showFavoriteFilm);
 }
 
-// КНОПКА НА ГОЛОВНОМУ ЕКРАНІ МОЇ УЛУБЛЕННІ
 const favorite = document.querySelector('.favorite');
 favorite.addEventListener('click', showFavoriteFilm);
 
-// ПОКАЗАННЯ УЛЮБЛЕННИХ ФІЛЬМІВ
 function showFavoriteFilm(e) {
   favorite.textContent = 'All Movies';
   refs.filmsList.innerHTML = '';
   getFilmsFavorite(idUser).then(result => {
     result.forEach(film => {
+      console.log(film);
+      console.log(film.title);
       film.idUser === idUser ? createListItem(film, true) : null;
     });
   });
