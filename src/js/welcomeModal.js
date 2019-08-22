7b5d8f56c061c5df19730b850af361b40ba30c39
Andrein <nykolynandrii@gmail.com>
2019-08-22 16:44:37 +0300

    fixes to search

--- src/js/welcomeModal.js
+++ src/js/welcomeModal.js
@@ -1,39 +1,33 @@
-import Swal from 'sweetalert2';
-import {
-    getUserName
-} from './services/api'
+import Swal from "sweetalert2";
+import { getUserName } from "./services/api";
 
- const welcomeModale = () => {
-    const welcomeId = sessionStorage.getItem('id') === null ? localStorage.getItem('key') : sessionStorage.getItem('id')
-    getUserName(welcomeId).then(user => {
-        Swal.fire({
-            title: `Welcome ${user.login}! `,
-            text: 'In your collection',
-            width: 600,
-            // animation: false,
-            showConfirmButton: false,
-            customClass: 'animated bounce',
-            timer: 10500,
-            type: 'success',
-            padding: '10em',
-            // background: '#fff url("http://www.coolwebmasters.com/uploads/posts/2010-10/1287573191_patterns-42.jpg")',
-            backdrop: `
+const welcomeModale = () => {
+  const welcomeId =
+    sessionStorage.getItem("id") === null
+      ? localStorage.getItem("key")
+      : sessionStorage.getItem("id");
+  getUserName(welcomeId).then(user => {
+    Swal.fire({
+      title: `Welcome ${user.login}! `,
+      text: "In your collection",
+      width: 600,
+      // animation: false,
+      showConfirmButton: false,
+      customClass: "animated bounce",
+      timer: 3000,
+      type: "success",
+      padding: "10em",
+      // background: '#fff url("http://www.coolwebmasters.com/uploads/posts/2010-10/1287573191_patterns-42.jpg")',
+      backdrop: `
             rgba(0,0,123,0.4)
             url("https://i.gifer.com/PYh.gif")
             center left
             no-repeat
-            `,
-        });
-    })
-}
-
-export const handleModalWelcome = () => {
-    welcomeModale()
+            `
+    });
+  });
 };
 
-// const open = document.getElementById('submit-signin')
-// if (localStorage.getItem('key')) {
-//     welcomeModale()
-// }
-
-// open.addEventListener('click', handleModalWelcome);
+export const handleModalWelcome = () => {
+  welcomeModale();
+};
